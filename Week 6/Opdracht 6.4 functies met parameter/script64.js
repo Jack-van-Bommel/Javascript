21462309efa30df9407c267519702a9df5a9028b
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-10-14 10:58:09 +0200

    js opdracht 6.4 afgemaakt

--- Week 6/Opdracht 6.4 functies met parameter/script64.js
+++ Week 6/Opdracht 6.4 functies met parameter/script64.js
@@ -3,3 +3,19 @@ function calculate(num,num2){
     console.log(answer);
     alert(answer);
 }
+
+function multiplybyfive(num){
+    let answer = num * 5;
+    console.log(answer);
+    alert(answer);
+}
+
+function calculateminutes(num){
+    let answer = num / 60;
+    console.log(answer);
+    alert(answer);
+}
+
+calculateminutes(180);
+multiplybyfive(100);
+calculate(5,100);
