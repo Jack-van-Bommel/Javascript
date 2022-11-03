a41162e18a673cbb1bd88e9e149f239269ccfba1
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-11-03 15:23:53 +0100

    eindelijk af. Breincellen do be gone

--- Week 8/Opdracht 8.1 Calculator/script.js
+++ Week 8/Opdracht 8.1 Calculator/script.js
@@ -1,27 +1,61 @@
 function multiply() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    var antwoord = getal1 * getal2;
-    document.getElementById("antwoord").innerHTML = antwoord;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 * getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
 }
 function divide() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    var antwoord = getal1 / getal2;
-    document.getElementById("antwoord").innerHTML = antwoord;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 / getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
 }
 
 function add() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    var antwoord = getal1 + getal2;
-    document.getElementById("antwoord").innerHTML = antwoord;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 + getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
 }
 
 function minus() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    var antwoord = getal1 - getal2;
-    document.getElementById("antwoord").innerHTML = antwoord;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 - getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
 }
+
+
 
