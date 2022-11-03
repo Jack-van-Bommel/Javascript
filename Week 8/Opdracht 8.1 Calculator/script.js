aa21609ceabde04eca3597d2c85b5b8866239445
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-11-03 14:45:28 +0100

    shit

---
+++ Week 8/Opdracht 8.1 Calculator/script.js
@@ -0,0 +1,15 @@
+function multiply() {
+    var getal1 = parseInt(window.document.getElementById("getal1")).value,    
+}
+function divide() {
+    
+}
+
+function add() {
+    
+}
+
+function minus() {
+    
+}
+
