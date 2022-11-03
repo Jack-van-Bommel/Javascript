0c80be8be2427927833af721410caf712ea37a4c
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-11-03 15:01:14 +0100

    het werkt. Mijn sanity is gone mijn rookverslaving is beter dan ooit.

--- Week 8/Opdracht 8.1 Calculator/script.js
+++ Week 8/Opdracht 8.1 Calculator/script.js
@@ -1,15 +1,27 @@
 function multiply() {
-    var getal1 = parseInt(window.document.getElementById("getal1")).value,    
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    var antwoord = getal1 * getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
 }
 function divide() {
-    
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    var antwoord = getal1 / getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
 }
 
 function add() {
-    
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    var antwoord = getal1 + getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
 }
 
 function minus() {
-    
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    var antwoord = getal1 - getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
 }
 
