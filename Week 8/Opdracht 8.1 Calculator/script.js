b93c024d3ea80323ca4fd55276605954092de5a9
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-11-04 11:06:39 +0100

    added || getal == 0 fail - fail

--- Week 8/Opdracht 8.1 Calculator/script.js
+++ Week 8/Opdracht 8.1 Calculator/script.js
@@ -1,59 +1,59 @@
 function multiply() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    if (getal1 < 0) {
+    if (getal1 < 0 || getal1 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     } 
-    else if (getal2 < 0) {
+    else if (getal2 < 0 || getal2 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     }
     else {
         var antwoord = getal1 * getal2;
-        document.getElementById("antwoord").innerHTML = antwoord;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is:" + antwoord;
     }
 }
 function divide() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    if (getal1 < 0) {
+    if (getal1 < 0 || getal1 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     } 
-    else if (getal2 < 0) {
+    else if (getal2 < 0 || getal2 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     }
     else {
         var antwoord = getal1 / getal2;
-        document.getElementById("antwoord").innerHTML = antwoord;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is:" + antwoord;
     }
 }
 
 function add() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    if (getal1 < 0) {
+    if (getal1 < 0 || getal1 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     } 
-    else if (getal2 < 0) {
+    else if (getal2 < 0 || getal2 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     }
     else {
         var antwoord = +getal1 + +getal2;
-        document.getElementById("antwoord").innerHTML = antwoord;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is:" +  antwoord;
     }
 }
 
 function minus() {
     var getal1 = document.getElementById("getal1").value;
     var getal2 = document.getElementById("getal2").value;
-    if (getal1 < 0) {
+    if (getal1 < 0 || getal1 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     } 
-    else if (getal2 < 0) {
+    else if (getal2 < 0 || getal2 == 0) {
         document.getElementById("antwoord").innerHTML = "Getal te laag";
     }
     else {
         var antwoord = getal1 - getal2;
-        document.getElementById("antwoord").innerHTML = antwoord;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is:" +  antwoord;
     }
 }
 
